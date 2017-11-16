6a92ac02d22561b128a1d97b9ceffc5c95caf578
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-11-16 14:39:22 +0100

    add hostname to http response and startup message

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	log.Println("starting hellohttp ...")
 	router := mux.NewRouter()
 	router.HandleFunc("/", index).Methods("GET")
 	router.HandleFunc("/health", health).Methods("GET")
@@ -18,7 +20,8 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.UserAgent())
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "hello http")
+	h, _ := os.Hostname()
+	fmt.Fprintf(w, "%s > hello http\n", h)
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
